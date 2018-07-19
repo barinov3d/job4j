8b2d59264b9672549d53a6b21d41ec40ca493ec0
barinov3d <barinov3d@gmail.com>
2018-07-19 20:07:37 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,10 @@
  package ru.job4j.calculate;
  
+	/**
+	* Prints out "Hello World!" message
+	* 
+	*@param args args
+	*/
  public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello World!");
