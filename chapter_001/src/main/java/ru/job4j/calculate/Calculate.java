c90f76bdf76aba15c20ee53567c2a78449fa576d
barinov3d <barinov3d@gmail.com>
2018-07-19 20:47:32 +0400

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,5 +9,14 @@
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+	
+	/**
+	* Echo method
+	* @param name your name
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
  }
  
