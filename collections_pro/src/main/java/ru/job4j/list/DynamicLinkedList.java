ac4388e36fe3b3090604f7653b55ac546eab1cdb
barinov3d <barinov3d@gmail.com>
2018-08-06 12:35:00 +0400

    5.3.2. Создать контейнер на базе связанного списка

---
+++ collections_pro/src/main/java/ru/job4j/list/DynamicLinkedList.java
@@ -0,0 +1,87 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс DynamicLinkedList.
+ */
+public class DynamicLinkedList<E extends Number> implements Iterable<E> {
+
+    private int size;
+    private Node<E> first;
+    private Node<E> node;
+    int modCount = 0;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E value) {
+        Node<E> newLink = new Node<>(value);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+        node = first;
+        modCount++;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.value;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+
+        E value;
+        Node<E> next;
+
+        Node(E value) {
+            this.value = value;
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int expectedModCount = 0;
+
+            @Override
+            public boolean hasNext() {
+                checkForComodification();
+                return node.next != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E result = node.value;
+                node = node.next;
+                return result;
+            }
+
+            final void checkForComodification() {
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
+            }
+        };
+    }
+}
