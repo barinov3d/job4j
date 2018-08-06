134fdfb8cc252a63b9ecb4a65c2e9520482eb767
barinov3d <barinov3d@gmail.com>
2018-08-06 11:15:48 +0400

    5.3.1. Создать динамический список на базе массива p2

--- collections_pro/src/main/java/ru/job4j/list/DynamicList.java
+++ collections_pro/src/main/java/ru/job4j/list/DynamicList.java
@@ -2,6 +2,7 @@ package ru.job4j.list;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Дмитрий on 04.08.2018.
@@ -10,18 +11,17 @@ public class DynamicList<E> implements Iterable<E> {
     private int size = 5;
     private int index = 0;
     int modCount = 0;
-    int expectedModCount = 0;
     Object[] container = new Object[size];
 
     public void add(E value) {
         if (index > size - 1) {
-            doubleConteinerSize();
+            doubleContainerSize();
         }
         container[index++] = value;
         modCount++;
     }
 
-    private void doubleConteinerSize() {
+    private void doubleContainerSize() {
         Object[] doubleContainer = new Object[size * 2];
         System.arraycopy(container, 0, doubleContainer, 0, size);
         container = doubleContainer;
@@ -41,24 +41,28 @@ public class DynamicList<E> implements Iterable<E> {
 
     @Override
     public Iterator<E> iterator() {
-        checkForComodification();
         return new Iterator<E>() {
+            int expectedModCount = 0;
+
             @Override
             public boolean hasNext() {
+                checkForComodification();
                 return index < size;
             }
 
             @Override
             public E next() {
-                modCount++;
-                expectedModCount++;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
                 return (E) container[index++];
+            }
+
+            final void checkForComodification() {
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
             }
         };
     }
 
-    final void checkForComodification() {
-        if (modCount != expectedModCount)
-            throw new ConcurrentModificationException();
-    }
 }
