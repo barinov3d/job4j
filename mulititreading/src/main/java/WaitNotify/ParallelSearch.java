0676349f4d0f7acdcc258e766a4941663bbc2f0f
barinov3d <barinov3d@gmail.com>
2018-10-25 12:39:00 +0400

    2. Обеспечить остановку потребителя. p1

---
+++ mulititreading/src/main/java/WaitNotify/ParallelSearch.java
@@ -0,0 +1,43 @@
+package WaitNotify;
+
+/**
+ * Created by Дмитрий on 25.10.2018.
+ */
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        );
+        producer.start();
+    }
+}
